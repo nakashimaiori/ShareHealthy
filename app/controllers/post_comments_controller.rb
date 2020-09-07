bebf31926f3dd119b5a762796e57a523f588fad9
vagrant <vagrant@localhost.localdomain>
2020-09-07 15:39:43 +0000

    [Fix]comment

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,15 +1,18 @@
 class PostCommentsController < ApplicationController
   def create
-  	post = Post.find(params[:post_id])
+  	@post = Post.find(params[:post_id])
   	comment = current_user.post_comments.new(post_comment_params)
-  	comment.post_id = post.id
+  	comment.post_id = @post.id
   	comment.save
-  	redirect_to post_path(post.id)
+  	# redirect_to post_path(post.id)
   end
 
   def destroy
-  	PostComment.find_by(id: params[:id], post_id: params[:post_id]).destroy
-    redirect_to post_path(params[:post_id])
+    @post = Post.find(params[:post_id])
+  	# PostComment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+    # redirect_to post_path(params[:post_id])
+    comment = PostComment.find(params[:id])
+    comment.destroy
   end
 
   private
