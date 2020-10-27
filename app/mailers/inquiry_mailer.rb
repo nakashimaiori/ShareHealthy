6e5b108ea4a8c6f3f66027194f837d8df8c9e5da
vagrant <vagrant@localhost.localdomain>
2020-10-27 13:15:51 +0000

    [Add]inquiry

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,9 +1,8 @@
 class InquiryMailer < ActionMailer::Base
-  default from: "example@example.com"   # 送信元アドレス
-  default to: "nakashima.0077@gmail.com"     # 送信先アドレス
+  default from: "example@example.com"
 
-  def received_email(inquiry)
-    @inquiry = inquiry
-    mail(:subject => 'お問い合わせを承りました')
-  end
+    def received_email(inquiry)
+        @inquiry = inquiry
+        mail(to: inquiry.email, bcc: ENV['USER_NAME'], subjext: 'お問い合わせを承りました')
+    end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,16 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: ENV['USER_NAME'],
+    password: ENV['PASSWORD'],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   config.action_mailer.perform_caching = false
 
