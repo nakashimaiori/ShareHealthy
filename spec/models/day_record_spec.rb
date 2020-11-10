5429288d6fd0e60d9a2813c8e652a3914208226e
vagrant <vagrant@localhost.localdomain>
2020-11-10 14:49:49 +0000

    [Add]test_day_record

--- spec/models/day_record_spec.rb
+++ spec/models/day_record_spec.rb
@@ -15,5 +15,35 @@ RSpec.describe Chat, day_record: :model do
       day_record.valid?
       expect(day_record.errors[:start_time]).to include("が入力されていません。")
     end
+
+    it "朝食が200文字以内でないと投稿できない" do
+      day_record = build(:day_record, breakfast: "a" * 201)
+      day_record.valid?
+      expect(day_record.errors[:breakfast]).to include("は200文字以下に設定して下さい。")
+    end
+
+    it "昼食が200文字以内でないと投稿できない" do
+      day_record = build(:day_record, lunch: "a" * 201)
+      day_record.valid?
+      expect(day_record.errors[:lunch]).to include("は200文字以下に設定して下さい。")
+    end
+
+    it "夕食が200文字以内でないと投稿できない" do
+      day_record = build(:day_record, dinner: "a" * 201)
+      day_record.valid?
+      expect(day_record.errors[:dinner]).to include("は200文字以下に設定して下さい。")
+    end
+
+    it "間食が200文字以内でないと投稿できない" do
+      day_record = build(:day_record, snack: "a" * 201)
+      day_record.valid?
+      expect(day_record.errors[:snack]).to include("は200文字以下に設定して下さい。")
+    end
+
+    it "メモが300文字以内でないと投稿できない" do
+      day_record = build(:day_record, memo: "a" * 301)
+      day_record.valid?
+      expect(day_record.errors[:memo]).to include("は300文字以下に設定して下さい。")
+    end
   end
 end
