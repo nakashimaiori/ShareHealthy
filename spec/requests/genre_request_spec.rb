7989a5b3efce01da09a759e634305a5e0f623994
vagrant <vagrant@localhost.localdomain>
2020-11-14 02:51:58 +0000

    [Add]test

--- spec/requests/genre_request_spec.rb
+++ spec/requests/genre_request_spec.rb
@@ -1,1 +1,14 @@
 require 'rails_helper'
+
+RSpec.describe "Genre", type: :request do
+  describe 'ジャンル新規一覧ページ' do
+    context "一覧が正しく表示される" do
+      before do
+        get admins_genres_path
+      end
+      it 'リクエストは200 OKとなること' do
+        expect(response.status).to eq 200
+      end
+    end
+  end
+end
