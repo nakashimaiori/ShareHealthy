77ca0d4d07b44d34b2e30ec64a4af494a470b06e
vagrant <vagrant@localhost.localdomain>
2020-11-16 15:56:39 +0000

    [Add]test_post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -55,5 +55,11 @@ RSpec.describe Post, type: :model do
         expect(Post.reflect_on_association(:user).macro).to eq :belongs_to
       end
     end
+
+    context 'PostCommrntモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(Post.reflect_on_association(:post_comments).macro).to eq :has_many
+      end
+    end
   end
 end
