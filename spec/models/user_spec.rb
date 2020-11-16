e3079ccf7cdf00df405074fb8a3261bc14ec0459
vagrant <vagrant@localhost.localdomain>
2020-11-16 16:10:01 +0000

    [Add]test_user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -53,5 +53,29 @@ RSpec.describe User, type: :model do
         expect(User.reflect_on_association(:posts).macro).to eq :has_many
       end
     end
+
+    context 'PostCommentモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:post_comments).macro).to eq :has_many
+      end
+    end
+
+    context 'Chatモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:chats).macro).to eq :has_many
+      end
+    end
+
+    context 'DayRecordモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:day_records).macro).to eq :has_many
+      end
+    end
+
+      context 'DayWeightモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:day_weights).macro).to eq :has_many
+      end
+    end
   end
 end
